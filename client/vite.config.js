58d0c5a8c2fcc03fcf87c0f8aec425e283bc5e85
stepleonard <stephanie.leonard716@gmail.com>
2026-04-16 10:23:06 -0700

    changed api targets

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://product-feedback-app-iurs.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
